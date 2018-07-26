8534fd226114e9560c4a49731be3df92801bac1e
ASUS <553728561@qq.com>
2018-07-26 16:14:09 +0800

    各个子Fragment的创建

---
+++ app/src/main/java/com/sky/app/shoppingmall/home/fragment/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.sky.app.shoppingmall.home.fragment;
+
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.sky.app.shoppingmall.base.BaseFragment;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created with Android Studio.
+ * 描述: 主页面的Fragment
+ * Date: 2018/7/26
+ * Time: 16:07
+ *
+ * @author 晏琦云
+ * @version ${VERSION}
+ */
+public class HomeFragment extends BaseFragment {
+    private TextView textView;
+
+    @Override
+    public View initView() {
+        Log.e(TAG, "主页面的Fragment的UI被初始化了");
+        textView = new TextView(mContext);
+        textView.setTextSize(25);
+        textView.setGravity(Gravity.CENTER);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        Log.e(TAG, "主页面的Fragment的数据被初始化了");
+        textView.setText("主页面内容");
+    }
+}
---
+++ app/src/main/java/com/sky/app/shoppingmall/shoppingcart/fragment/ShoppingCartFragment.java
@@ -0,0 +1,39 @@
+package com.sky.app.shoppingmall.shoppingcart.fragment;
+
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.sky.app.shoppingmall.base.BaseFragment;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created with Android Studio.
+ * 描述: 购物车的Fragment
+ * Date: 2018/7/26
+ * Time: 16:07
+ *
+ * @author 晏琦云
+ * @version ${VERSION}
+ */
+public class ShoppingCartFragment extends BaseFragment {
+    private TextView textView;
+
+    @Override
+    public View initView() {
+        Log.e(TAG, "购物车的Fragment的UI被初始化了");
+        textView = new TextView(mContext);
+        textView.setTextSize(25);
+        textView.setGravity(Gravity.CENTER);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        Log.e(TAG, "购物车的Fragment的数据被初始化了");
+        textView.setText("购物车内容");
+    }
+}
