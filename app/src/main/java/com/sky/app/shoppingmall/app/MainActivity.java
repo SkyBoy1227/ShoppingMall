34e61b4761c927af9372b9164c94ac2f7f25c476
ASUS <553728561@qq.com>
2018-07-26 15:33:42 +0800

    使用ButterKnife初始化布局

--- app/src/main/java/com/sky/app/shoppingmall/app/MainActivity.java
+++ app/src/main/java/com/sky/app/shoppingmall/app/MainActivity.java
@@ -3,8 +3,13 @@ package com.sky.app.shoppingmall.app;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 
 import com.sky.app.shoppingmall.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created with Android Studio.
@@ -16,9 +21,17 @@ import com.sky.app.shoppingmall.R;
  * @version ${VERSION}
  */
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.frameLayout)
+    FrameLayout frameLayout;
+    @BindView(R.id.rg_main)
+    RadioGroup rgMain;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        // ButterKnife和当前Activity绑定
+        rgMain.check(R.id.rb_home);
     }
 }
