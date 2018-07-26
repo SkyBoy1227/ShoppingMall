8653312797ab0ea189eb19ea9184f0d1b3489af9
ASUS <553728561@qq.com>
2018-07-26 12:02:54 +0800

    项目初始化

---
+++ app/src/main/java/com/sky/app/shoppingmall/WelcomeActivity.java
@@ -0,0 +1,22 @@
+package com.sky.app.shoppingmall;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created with Android Studio.
+ * 描述: 欢迎页
+ * Date: 2018/7/26
+ * Time: 11:40
+ *
+ * @author 晏琦云
+ * @version ${VERSION}
+ */
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
